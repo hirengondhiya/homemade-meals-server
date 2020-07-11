c79e4869f1e94d431a279fd7649f7ed63d385542
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-11 15:47:42 +1000

    added basis web server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3010
+
+app.get("/", (req, res) => {
+  res.send("homemade meals api!")
+})
+
+app.listen(port, () => console.log(`Homemade meals server listening on port ${port}`))
