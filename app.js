97a1918d3b0251d58269a4685653b663fbe20e26
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-12 07:58:19 +1000

    added body-parser

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const port = process.env.PORT || 3010
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+
 app.get("/", (req, res) => {
   res.send("homemade meals api!")
+})
+
+app.post("/", (req, res) => {
+  res.send(req.body)
 })
 
 app.listen(port, () => console.log(`Homemade meals server listening on port ${port}`))
