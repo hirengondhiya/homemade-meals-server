432be18959ebc6af2757bbf07c264c9b0dd952f0
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-11 16:14:21 +1000

    fixed PORT typo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3010
+const port = process.env.PORT || 3010
 
 app.get("/", (req, res) => {
   res.send("homemade meals api!")
