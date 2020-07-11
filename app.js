e3cdfc7599e6080e52d75894eea7c80ca6fee0a5
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-12 07:51:55 +1000

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3010
 
+const app = express()
+app.use(cors())
 app.get("/", (req, res) => {
   res.send("homemade meals api!")
 })
