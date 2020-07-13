2022cef341460aa56f7659985fde7d9db5f91382
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-14 08:18:02 +1000

    to run auto fix using prettier

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-require('./db/connect-db')
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const menuRoutes = require('./routes/menu-routes')
+require("./db/connect-db");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const menuRoutes = require("./routes/menu-routes");
 
-const port = process.env.PORT || 3010
+const port = process.env.PORT || 3010;
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("homemade meals api!")
-})
-app.use("/menu", menuRoutes)
+  res.send("homemade meals api!");
+});
+app.use("/menu", menuRoutes);
 
-app.listen(port, () => console.log(`Homemade meals server listening on port ${port}`))
+app.listen(port, () =>
+  console.log(`Homemade meals server listening on port ${port}`)
+);
--- routes/menu-routes.js
+++ routes/menu-routes.js
@@ -1,14 +1,21 @@
-const { Router } = require('express')
-const {createMenuItem, getAllMenuItems, getMenuItem, getMenuOfTheDay, updateMenuItem, deleteMenuItem } = require('../controllers/menu-controller')
+const { Router } = require("express");
+const {
+  createMenuItem,
+  getAllMenuItems,
+  getMenuItem,
+  getMenuOfTheDay,
+  updateMenuItem,
+  deleteMenuItem,
+} = require("../controllers/menu-controller");
 
-const menuRoutes = Router()
+const menuRoutes = Router();
 
-menuRoutes.get("/", getAllMenuItems)
-menuRoutes.get("/oftheday",getMenuOfTheDay)
-menuRoutes.get("/:id", getMenuItem)
+menuRoutes.get("/", getAllMenuItems);
+menuRoutes.get("/oftheday", getMenuOfTheDay);
+menuRoutes.get("/:id", getMenuItem);
 
-menuRoutes.post("/", createMenuItem)
-menuRoutes.put("/:id", updateMenuItem)
-menuRoutes.delete("/:id", deleteMenuItem)
+menuRoutes.post("/", createMenuItem);
+menuRoutes.put("/:id", updateMenuItem);
+menuRoutes.delete("/:id", deleteMenuItem);
 
-module.exports = menuRoutes
+module.exports = menuRoutes;
