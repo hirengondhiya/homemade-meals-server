2cb24733c4910f09bb9d39acd66c00de686fcedf
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-15 21:53:22 +1000

    defined create order

--- utilities/order_utility.js
+++ utilities/order_utility.js
@@ -1,22 +1,19 @@
 const Menu = require("../models/menu");
 
 const createOrder = async (menuId, order) => {
-  try {
-    const updatedMenu = await Menu.findByIdAndUpdate(
-      menuId,
-      {
-        $push: {
-          orders: order,
-        },
+  const updatedMenu = await Menu.findByIdAndUpdate(
+    menuId,
+    {
+      $push: {
+        orders: order,
       },
-      {
-        new: true,
-      }
-    ).exec();
-    return updatedMenu;
-  } catch (err) {
-    throw new Error("Cannot create order for provided menuId");
-  }
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).exec();
+  return updatedMenu;
 };
 
 module.exports = {
