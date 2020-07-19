999376ccda1a30cbe5791eddd4f02d2c1cebb8ea
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-19 11:13:50 +1000

    auth controller

---
+++ controllers/auth_controller.js
@@ -0,0 +1,61 @@
+const passport = require("passport");
+const User = require("../models/user");
+const { badRequest, internalServerError } = require("./controller-utils");
+const authenticate = passport.authenticate("local");
+// helper function
+
+function loginUser(req, res) {
+  try {
+    authenticate(req, res, function (err, result) {
+      if (err) {
+        return badRequest(req, res, err);
+      }
+      // See what we have
+      console.log("authenticated", req.user.username);
+      console.log("session object:", req.session);
+      console.log("req.user:", req.user);
+      console.log(result);
+      // send back the user
+      const { username, email, role } = req.user;
+      res.json({ username, email, role });
+    });
+  } catch (err) {
+    internalServerError(req, res, err);
+  }
+}
+
+const register = function (req, res) {
+  try {
+    const { username, password, email, role } = req.body;
+    User.register(
+      new User({
+        username,
+        email,
+        role,
+      }),
+      password,
+      function (err) {
+        if (err) {
+          return badRequest(req, res, err);
+        }
+        loginUser(req, res);
+      }
+    );
+  } catch (err) {
+    internalServerError(req, res, err);
+  }
+};
+
+const logout = function (req, res) {
+  try {
+    console.log(`logging out user ${req.user}`);
+    req.logout();
+    console.log("session object:", req.session);
+    console.log("req.user:", req.user);
+    res.sendStatus(200);
+  } catch (err) {
+    internalServerError(req, res, err);
+  }
+};
+
+module.exports = { register, login: loginUser, logout };
