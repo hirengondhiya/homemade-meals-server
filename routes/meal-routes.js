156628d37dc2f34b467130c8b84f03feb15937bf
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-07-19 11:31:45 +1000

    routes with authorization

--- routes/meal-routes.js
+++ routes/meal-routes.js
@@ -1,29 +1,38 @@
 const { Router } = require("express");
 const {
   createMealItem,
-  getAllMealItems,
-  getMealItem,
+  // getAllMealItems,
+  // getMealItem,
   getMealsOpenForOrders,
+  getAllMealsSoldByMe,
+  getMealSoldByMe,
   updateMealItem,
   deleteMealItem,
 } = require("../controllers/meals-controller");
+const { userAuthenticated, verifyMealSeller } = require("./middleware");
 
-const {
-  getOrdersByMeal,
-  createOrderForMeal,
-} = require("../controllers/orders-controller");
+// const {
+//   getOrdersByMeal,
+//   createOrderForMeal,
+// } = require("../controllers/orders-controller");
 
 const mealRoutes = Router();
 
-mealRoutes.get("/", getAllMealItems);
 mealRoutes.get("/openfororders", getMealsOpenForOrders);
-mealRoutes.get("/:id", getMealItem);
 
+// mealRoutes.get("/", getAllMealItems);
+// mealRoutes.get("/:id", getMealItem);
+mealRoutes.use(userAuthenticated);
+mealRoutes.get("/", getAllMealsSoldByMe);
 mealRoutes.post("/", createMealItem);
+
+mealRoutes.use("/:id", verifyMealSeller);
+
+mealRoutes.get("/:id", getMealSoldByMe);
 mealRoutes.put("/:id", updateMealItem);
 mealRoutes.delete("/:id", deleteMealItem);
 
-mealRoutes.get("/:id/orders", getOrdersByMeal);
-mealRoutes.post("/:id/orders", createOrderForMeal);
+// mealRoutes.get("/:id/orders", getOrdersByMeal);
+// mealRoutes.post("/:id/orders", createOrderForMeal);
 
 module.exports = mealRoutes;
